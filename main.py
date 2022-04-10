7d09b85f5656557430a2148b0e804e78f92340e3
Techshwift <techshwift@gmail.com>
2022-04-10 18:22:27 -0400

    implemented buttons

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('~/Projects/smartcar/modules/freenove')
+import json
+sys.path.append('/home/pi/Projects/smartcar/modules/freenove')
 from flask import Flask,send_from_directory
 
 app = Flask(__name__)
@@ -17,18 +18,23 @@ def move_motor(action):
     if action == 'forward':
         motor.setMotorModel(1000,1000,1000,1000)       #Forward
         print ("The car is moving forward")
+        return json.dumps({'status': 'Forward'})
     elif action == 'reverse':
         motor.setMotorModel(-1000,-1000,-1000,-1000)   #Back
         print ("The car is going backwards")    
+        return json.dumps({'status': 'Reverse'})
     elif action == 'left':
         motor.setMotorModel(-1500,-1500,2000,2000)       #Left 
         print ("The car is turning left")  
+        return json.dumps({'status': 'Left'})
     elif action == 'right':
         motor.setMotorModel(2000,2000,-1500,-1500)       #Right 
         print ("The car is turning right")  
+        return json.dumps({'status': 'Right'})
     else:
         motor.setMotorModel(0,0,0,0)                   #Stop
         print ("The car is stopped")
+        return json.dumps({'status': 'Stop'})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
